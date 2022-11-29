6053667ec68e0610790450e165133e85d1dd12ba
rafliAhmad18 <115543638+rafliAhmad18@users.noreply.github.com>
2022-11-29 19:10:48 +0700

    feat:initial commit

---
+++ AplikasiInputBarang/src/sp_daftarbarang.java
@@ -0,0 +1,20 @@
+
+import com.stripbandunk.jwidget.JDynamicTable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+class sp_daftarbarang {
+
+    static void setViewportView(JDynamicTable jDynamicTable) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
